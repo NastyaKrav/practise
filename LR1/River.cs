42b48156c4de49482f21f25fb6ec21dd3ee1f06a
NastyaKrav <nkravcenok@gmail.com>
2021-03-15 18:50:56 +0300

    3ий коммит

---
+++ LR1/River.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LR1
+{
+    class River
+    {
+        public string name { get; set; }
+        public double square { get; set; }
+        public double length { get; set; }
+
+    }
+}
